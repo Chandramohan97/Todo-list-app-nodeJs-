21706927eb8621d894a679ae481a2219631f567e
Chandramohan97 <chandramsriv@gmail.com>
2023-02-16 17:57:40 +0530

    feat : exporting Login and signUp model.

--- model/model.js
+++ model/model.js
@@ -1,13 +1,21 @@
 const mongoose = require("mongoose");
 
-const login = new mongoose.Schema({
+const loginSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
 });
 
-const signUp = new mongoose.Schema({
+const signUpSchema = new mongoose.Schema({
   fullname: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, unique: true },
   mobileNo: { type: Number, required: true, unique: true },
 });
+
+const Login = mongoose.model("Login", login);
+const signUp = mongoose.model("signUpSchema", signUpSchema);
+
+module.exports = {
+  Login,
+  signUp,
+};
